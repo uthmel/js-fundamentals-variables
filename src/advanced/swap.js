415558eac7b3a9a488bfc68bd880df88e45a41d6
uthmel <melvin.uthayaseelan@no.experis.com>
2024-09-20 09:55:35 +0200

    everything finished

--- src/advanced/swap.js
+++ src/advanced/swap.js
@@ -1,6 +1,8 @@
-let a = 8
-let b = 10
+let a = 8;
+let b = 10;
 
 // TODO: Swap the values of a and b without changing lines 1 and 2; extra points if you can do it without using a temporary variable
+
+[a,b] = [b,a]
 
 module.exports = { a, b }
